917ed141c75a1cc2e7a4e53a0acf884d1a36caf3
vitaliiromanko <r.vitalii.v030@gmail.com>
2025-09-15 16:36:12 +0300

    Fixed mistakes

--- src/main/java/com/portfolio/lmsbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/portfolio/lmsbackend/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.portfolio.lmsbackend.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -15,10 +17,20 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
+
+        List<String> globalErrors = ex.getBindingResult().getGlobalErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .toList();
+
+        if (!globalErrors.isEmpty()) {
+            errors.put("globalErrors", globalErrors);
+        }
+
         return new ResponseEntity<>(errors, BAD_REQUEST);
     }
 }
